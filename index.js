3e99ab3ddc6613bdd3bb8d35850a9726065d2a00
Víctor Martínez <ahal_tocob@hotmail.com>
2025-09-12 15:07:19 -0600

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+//inicio conexion mongo
+const MongoClient = require('mongodb').MongoClient;
+//const url = "mongodb://root:123456@localhost:27017/"; // Cambia esto si usas Mongo Atlas
+const url = "mongodb+srv://superiorviktor:4hzYZt1lSTcBCycU@cluster0.53l4ojb.mongodb.net/"; // Cambia esto si usas Mongo Atlas
+//const url = process.env.urlMongo;
+const client = new MongoClient(url, {
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+});
+
+async function obtenerTodosAsync() {
+    await client.connect();
+    const db = client.db("Utilidades");
+    const collection = db.collection("transacciones");
+    let lista = await collection.find({}).toArray()
+
+    return lista
+}
+//fin conexion mongo
+
+app.get("/api/transactions", async (req, res) => {
+    let transactions = []
+    let lista = await obtenerTodosAsync()
+    lista.forEach(item => {
+        transactions.push({
+            transacciones: item.transacciones,
+            medioPago: "20240300570905|9636998|1|1|ARS|E|1843.99||1843.99|1.000000|100|N|2022-01-14|F|Y",
+            afip: "",
+            reversa: "",
+            snowflake: "WU_ID;123;5826000;33350017"
+        })
+    })
+
+    res.status(200).json(transactions)
+})
+
+app.listen(port, function () {
+    console.log("http://localhost:" + port)
+})
